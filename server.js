78410939fbbab55f2b276fc3a2552715280d1194
TeacherSpaceSchool <azyk.store@gmail.com>
2021-10-20 13:50:26 +0600

    check

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const next = require('next')
 const app = next({ dev: !!process.env.dev })
 const handle = app.getRequestHandler()
 
+console.log(1)
 app.prepare()
     .then(() => {
+        console.log(2)
         createServer((req, res) => {
+            console.log(3)
             const parsedUrl = parse(req.url, true)
             const { pathname } = parsedUrl
+            console.log(4)
 
             if (pathname === '/service-worker.js') {
                 const filePath = join(__dirname, '.next', pathname)
